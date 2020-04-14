c984d4d65ce4f48f1f5c5038a3197d139a2a56ff
Luis Fernandez <luis.fernandez2093@gmail.com>
2020-04-14 16:45:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,9 @@ def square_array(array)
   # Return a new array of the results
   
   array.map do |element| 
-    
+  puts " #{element.capitalize}!"
+end 
+
 end
 
 def summon_captain_planet(planeteer_calls)
